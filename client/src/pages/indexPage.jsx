668d08a7fbbf18470fcc0cf0a48a098a1ada5055
Oussama Fajraoui <102801929+Oussama-Fajraoui@users.noreply.github.com>
2023-02-09 12:21:19 +0100

    adding the link to login page

--- client/src/pages/indexPage.jsx
+++ client/src/pages/indexPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const indexPage = () => {
   return (
@@ -23,7 +24,7 @@ const indexPage = () => {
 </svg>
         </button>
       </div>
-      <div className='flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 '>
+      <Link to={'login'} className='flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 '>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
 </svg>
@@ -32,7 +33,7 @@ const indexPage = () => {
 <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clipRule="evenodd" />
 </svg>
 </div>
-      </div>
+      </Link>
     </header>
   </div>
   )
