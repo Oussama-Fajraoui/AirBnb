c02888f79600acfeffe7cb00bb78a450ee1adb97
Oussama Fajraoui <102801929+Oussama-Fajraoui@users.noreply.github.com>
2023-02-09 12:39:19 +0100

    putting the header in single page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import IndexPage from './pages/indexPage'
+import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
 
 function App() {
